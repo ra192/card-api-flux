0b21c2aaf52cc4a1a800c6d817ac8ec09e954d66
ra192 <yakovlev.roman@gmail.com>
2021-07-13 01:23:30 +0300

    minor changes

--- src/main/java/com/card/repository/TransactionItemRepository.java
+++ src/main/java/com/card/repository/TransactionItemRepository.java
@@ -1,11 +1,9 @@
 package com.card.repository;
 
 import com.card.entity.TransactionItem;
-import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Mono;
 
 public interface TransactionItemRepository extends ReactiveCrudRepository<TransactionItem, Long> {
-    @Query("select sum(amount) from transaction_item where account_id=:accountId")
-    Mono<Long> sumByAccount(Long accountId);
+    Mono<Long> findSumAmountByAccountId(Long accountId);
 }
