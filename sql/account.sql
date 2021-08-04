296391c6b5ab4cfa446e964926ad5a96d8d422d2
ra192 <yakovlev.roman@gmail.com>
2021-08-04 19:37:28 +0300

    add src and dest accounts for transactions

--- sql/account.sql
+++ sql/account.sql
@@ -1,4 +1,4 @@
-INSERT INTO public.account (id, active, currency, name, merchant_id) VALUES (1, true, 'USD', 'Cash account', 1);
-INSERT INTO public.account (id, active, currency, name, merchant_id) VALUES (2, true, 'USD', 'Card account', 1);
-INSERT INTO public.account (id, active, currency, name, merchant_id) VALUES (3, true, 'USD', 'Fee account', 1);
-INSERT INTO public.account (id, active, currency, name, merchant_id) VALUES (4, true, 'USD', 'Wayne USD account', 2);
+INSERT INTO public.account (id, active, currency, name, merchant_id, balance) VALUES (1, true, 'USD', 'Cash account', 1, 0);
+INSERT INTO public.account (id, active, currency, name, merchant_id, balance) VALUES (2, true, 'USD', 'Card account', 1, 0);
+INSERT INTO public.account (id, active, currency, name, merchant_id, balance) VALUES (3, true, 'USD', 'Fee account', 1, 0);
+INSERT INTO public.account (id, active, currency, name, merchant_id, balance) VALUES (4, true, 'USD', 'Wayne USD account', 2, 0);
