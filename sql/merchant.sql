33bccaa4c085c341b977d7be70a0904fac4bc969
ra192 <yakovlev.roman@gmail.com>
2021-08-05 00:36:26 +0300

    add sql schema

--- sql/merchant.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO public.merchant (id, name, secret) VALUES (1, 'Internal', 'C5X+rivjAkiczaDqkDwMKAVxN+cSGrudaGCRhWZeVos=');
-INSERT INTO public.merchant (id, name, secret) VALUES (2, 'Wayne Enterprise', '/05W5ibIDcNPkHXS8mC71CKB+10Ct9gzJ41C9HRMYeQ=');
